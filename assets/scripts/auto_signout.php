01202e9a8eca858e1cfca9b4d289e7b04546db9d
k-a-r-l-o <162566291+k-a-r-l-o@users.noreply.github.com>
2025-01-03 11:46:10 +0800

    auto

--- assets/scripts/auto_signout.php
+++ assets/scripts/auto_signout.php
@@ -2,37 +2,32 @@
 session_start(); // Start the session
 require_once '../../config/database.php';
 
-
 try {
     $db = new Database();
     $conn = $db->getConnection();
 
-    // Mark user as "Offline"
+    // Mark client users as "Offline" if inactive for more than 4 minutes
     $updateStatusStmt = $conn->prepare(
         "UPDATE tb_client_logindetails 
-            SET user_status = 'Offline' 
-                user_log = NULL, 
-            WHERE TIMESTAMPDIFF(MINUTE, user_log, NOW()) > 4"
+        SET user_status = 'Offline', user_log = NULL 
+        WHERE TIMESTAMPDIFF(MINUTE, user_log, NOW()) > 6"
     );
     $updateStatusStmt->execute();
 } catch (PDOException $e) {
-    error_log("Signout error: " . $e->getMessage());
-    // Optionally, you can show a message or log this for further review
+    error_log("Signout error for tb_client_logindetails: " . $e->getMessage());
 }
 
 try {
     $db = new Database();
     $conn = $db->getConnection();
 
-    // Mark user as "Offline"
+    // Mark admin users as "Offline" if inactive for more than 4 minutes
     $updateStatusStmt = $conn->prepare(
         "UPDATE tb_admin_logindetails 
-            SET user_status = 'Offline' 
-                user_log = NULL, 
-            WHERE TIMESTAMPDIFF(MINUTE, user_log, NOW()) > 4"
+        SET user_status = 'Offline', user_log = NULL 
+        WHERE TIMESTAMPDIFF(MINUTE, user_log, NOW()) > 6"
     );
     $updateStatusStmt->execute();
 } catch (PDOException $e) {
-    error_log("Signout error: " . $e->getMessage());
-    // Optionally, you can show a message or log this for further review
+    error_log("Signout error for tb_admin_logindetails: " . $e->getMessage());
 }
