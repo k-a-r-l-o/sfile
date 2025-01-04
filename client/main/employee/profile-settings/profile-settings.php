001f23afb2a0e262cb752e140ed6ac96ced51b24
k-a-r-l-o <162566291+k-a-r-l-o@users.noreply.github.com>
2025-01-05 01:07:05 +0800

    Revert "profile sessions"
    This reverts commit f8e5292cfce54d1239056fb9a6cee0392c3b6799.

--- client/main/employee/profile-settings/profile-settings.php
+++ client/main/employee/profile-settings/profile-settings.php
@@ -5,11 +5,9 @@ require_once '../../../../config/database.php';
 // Check if the session contains a user ID
 if (!isset($_SESSION['client_role'], $_SESSION['client_token'], $_SESSION['client_user_id'])) {
   header("Location: ../../../login?error=session_expired");
-  exit;
 } else {
   if ($_SESSION['client_role'] == 'Head') {
     header("Location: ../../head/");
-    exit;
   }
 }
 
